acf62e20783ef809f6febcde3bd47daf53c2ffbe
iprojas <rojas.ip@gmail.com>
2021-04-24 02:08:49 -0400

    🔥

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ function Deck() {
       const scale = down ? 1.1 : 1 // Active cards lift up a bit
       return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
     })
-
-    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set((i) => to(i)), 600)
+    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set((i) => to(i)), 6000)
+  })
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
   return props.map(({ x, y, rot, scale }, i) => (
     <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
